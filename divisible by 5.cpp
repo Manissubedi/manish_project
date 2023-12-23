81513d43eb7a57a1722143f894ac431be41098a6
Manish Subedi <154537115+Manissubedi@users.noreply.github.com>
2023-12-23 13:40:38 +0545

    Add files via upload

---
+++ divisible by 5.cpp
@@ -0,0 +1,16 @@
+//Write a program tha t accepts a character and check whether it is vowel or consonent?//
+
+#include <stdio.h>                                                                 //Header file//
+int main()                                                                       // Function//
+{
+	char ch;                                                                       // decleration of vaeiable//
+	printf("enter a character");
+	scanf("%c",&ch);
+	if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+	    ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')               //expression//
+	     
+	printf("%c is vowel",ch);
+	else
+	printf("%C is consunent",ch);
+	return  0;
+}
